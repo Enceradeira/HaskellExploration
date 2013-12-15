d6f5f189413ef07565900220e33a6a1e5240ec30
Jorg Jenni <joerg.jenni@garaio.com>
2013-12-15 17:09:52 +0000

    exercise chapter 3 added

---
+++ chapter3/src/Main.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE CPP, TemplateHaskell #-}
+-----------------------------------------------------------------------------
+--
+-- Module      :  Main
+-- Copyright   :
+-- License     :  AllRightsReserved
+--
+-- Maintainer  :
+-- Stability   :
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module Main (
+    main
+) where
+
+import Exercise
+
+main = do
+    putStrLn (show (toPalindrome [1,2,3]))
+
