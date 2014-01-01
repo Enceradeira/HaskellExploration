24f376bf49d7d168d9d318c419480abac29f4faa
Jörg Jenni <joerg.jenni@garaio.com>
2014-01-01 15:38:52 +0000

    Chapter 4 initialized

---
+++ chapter4/src/InteractWith.hs
@@ -0,0 +1,23 @@
+-----------------------------------------------------------------------------
+--
+-- Module      :  InteractWith
+-- Copyright   :
+-- License     :  AllRightsReserved
+--
+-- Maintainer  :
+-- Stability   :
+-- Portability :
+--
+-- |
+--
+-----------------------------------------------------------------------------
+
+module InteractWith (
+    main
+) where
+
+main = do
+    putStrLn "Hello"
+
+
+
